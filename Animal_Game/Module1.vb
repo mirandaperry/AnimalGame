2b32f7e9d5e2ada4fd0d5d2a32ec06231b882e9e
Miranda Perry <miranda.perry@ops-solutions.com>
2020-01-22 21:26:23 -0500

    Initial Commit

---
+++ Animal_Game/Module1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Module AnimalGame
+
+    Public HeadNode As Node
+    Public CurrentNode As Node
+
+    Public Const FILE_NAME = "store.json"
+    Public Function StoreFileExists() As Boolean
+        Return File.Exists(FILE_NAME)
+    End Function
+
+    Public Sub Main()
+        If Not StoreFileExists() Then
+            HeadNode = New Node("A DOG")
+            File.WriteAllText(FILE_NAME, JsonConvert.SerializeObject(HeadNode))
+        End If
+
+
+        While True
+            HeadNode = JsonConvert.DeserializeObject(Of Node)(File.ReadAllText(FILE_NAME))
+            CurrentNode = HeadNode
+            Console.WriteLine("THINK OF AN ANIMAL. I'LL TRY TO FIND IT OUT BY ASKING QUESTIONS")
+            Dim inGame = True
+            While inGame
+            Dim answer As String
+            If CurrentNode.isAnswer = False Then
+                Console.WriteLine(CurrentNode.Question)
+            Else
+                    Console.WriteLine($"IS IT {CurrentNode.Answer}?")
+                End If
+            answer = Console.ReadLine().ToUpper()
+                If answer = "YES" Then
+                    If CurrentNode.isAnswer Then
+                        Console.WriteLine($"SEE HOW SMART I'M GETTING?")
+                        inGame = False
+                    Else
+                        CurrentNode = CurrentNode.YesNode
+                    End If
+                ElseIf answer = "NO" Then
+                    If CurrentNode.isAnswer Then
+                        Console.WriteLine($"I GIVE UP! WHAT IS IT?")
+                        Dim newAnswer = Console.ReadLine.ToUpper
+                        Console.WriteLine($"PLEASE TYPE IN A QUESTION WHOSE ANSWER IS YES FOR {newAnswer} AND NO FOR {CurrentNode.Answer}")
+                        Dim newQuestion = Console.ReadLine.ToUpper.Replace("?", "") & "?"
+                        CurrentNode.AddQuestion(newAnswer, newQuestion)
+                            Console.WriteLine($"I'LL DO BETTER NEXT TIME")
+                            File.WriteAllText(FILE_NAME, JsonConvert.SerializeObject(HeadNode))
+                            inGame = False
+                        Else
+                            CurrentNode = CurrentNode.NoNode
+                    End If
+                Else
+                    Console.WriteLine("PLEASE ANSWER YES/NO")
+                End If
+            End While
+        End While
+    End Sub
+End Module
---
+++ Animal_Game/Node.vb
@@ -0,0 +1,21 @@
+﻿Public Class Node
+
+    Public isAnswer As Boolean
+    Public YesNode As Node
+    Public NoNode As Node
+    Public Question As String
+    Public Answer As String
+
+    Public Sub New(Answer As String)
+        Me.Answer = Answer
+        Me.isAnswer = True
+    End Sub
+
+    Public Sub AddQuestion(NewAnswer As String, NewQuestion As String)
+        isAnswer = False
+        Question = NewQuestion
+        NoNode = New Node(Answer)
+        YesNode = New Node(NewAnswer)
+    End Sub
+
+End Class
